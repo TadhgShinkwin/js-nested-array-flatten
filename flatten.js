c5a4ab4aeae38164caca5fe2297d398b86a4bc9a
TadhgShinkwin <tadhgcode@gmail.com>
2021-12-15 20:29:40 +0000

    Created initial test Data

---
+++ flatten.js
@@ -0,0 +1,13 @@
+//TestData:
+const testArray1 = [1, 2, 3, 4];
+const testArray2 = [1, [2, 3], [4, [5, 6]], 7];
+const testArray3 = [23, [40], 36, [12, 13, [45, 76, [80, 82, [91, 92], 29]], 13]];
+const testArray4 = [100, [200, [300, [400, [500, 600], 700], 800], 900], 1000];
+const testArray5 = [a, [b, c], [d, [e, f], g], h, i];
+
+//Flatten function: 
+
+
+
+
+//Application of flatten function using test data:
