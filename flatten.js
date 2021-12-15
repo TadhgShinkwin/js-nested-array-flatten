72fa6b2c75be7193b166a7b0aaa0aaf29289a87e
TadhgShinkwin <tadhgcode@gmail.com>
2021-12-15 20:37:57 +0000

    Wrote test cases & Fixed input data for testArray5 - to strings

--- flatten.js
+++ flatten.js
@@ -3,11 +3,63 @@ const testArray1 = [1, 2, 3, 4];
 const testArray2 = [1, [2, 3], [4, [5, 6]], 7];
 const testArray3 = [23, [40], 36, [12, 13, [45, 76, [80, 82, [91, 92], 29]], 13]];
 const testArray4 = [100, [200, [300, [400, [500, 600], 700], 800], 900], 1000];
-const testArray5 = [a, [b, c], [d, [e, f], g], h, i];
+const testArray5 = ["a", ["b", "c"], ["d", ["e", "f"], "g"], "h", "i"];
 
 //Flatten function: 
+const flatten = (arr) => {
+    let resultArray = [];
 
+    //iterate through array and recursively flatten as subarrays are detected
+
+    return resultArray;
+}
 
 
 
 //Application of flatten function using test data:
+
+const resultArray1 = flatten(testArray1);
+const resultArray2 = flatten(testArray2);
+const resultArray3 = flatten(testArray3);
+const resultArray4 = flatten(testArray4);
+const resultArray5 = flatten(testArray5);
+
+//Test-case 1
+console.log("--TestArray1--");
+console.log("BEFORE:");
+console.log(testArray1);
+console.log("AFTER:");
+console.log(resultArray1);
+console.log("-------------");
+
+//Test-case 2
+console.log("--TestArray2--");
+console.log("BEFORE:");
+console.log(testArray2);
+console.log("AFTER:");
+console.log(resultArray2);
+console.log("-------------");
+
+//Test-case 3
+console.log("--TestArray3--");
+console.log("BEFORE:");
+console.log(testArray3);
+console.log("AFTER:");
+console.log(resultArray3);
+console.log("-------------");
+
+//Test-case 4
+console.log("--TestArray4--");
+console.log("BEFORE:");
+console.log(testArray4);
+console.log("AFTER:");
+console.log(resultArray4);
+console.log("-------------");
+
+//Test-case 5
+console.log("--TestArray5--");
+console.log("BEFORE:");
+console.log(testArray5);
+console.log("AFTER:");
+console.log(resultArray5);
+console.log("-------------");
